c2f3d3b65e7ddcff387c5d796ccd9ee82f39f26b
Moises Jr <mdeosjr@gmail.com>
2025-05-21 23:03:11 -0300

    fix: update health check route to include API prefix; remove unused MongoDBRepository file

--- src/api/interface/routes/index.ts
+++ src/api/interface/routes/index.ts
@@ -7,14 +7,16 @@ import { createUploadRouter } from './upload';
 
 export function createApiRouter(db: Connection, messaging: IMessagingService): ExpressRouter {
   const router = Router();
+  
   router.use('/api', createStatusRouter(db));
   router.use('/api', createUploadRouter(db, messaging));
-  router.get('/health', (_, res, next) => {
+  router.get('/api/health', (_, res, next) => {
     try {
       res.status(200).send('OK');
     } catch (err) {
       next(err);
     }
   });
+
   return router;
 }
